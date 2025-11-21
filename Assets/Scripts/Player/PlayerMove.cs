3a9b2b0e4e1bbe192f0a84daeaaf6988f93b55c0
GreedSpirit <dlwjdgus0918@naver.com>
2025-11-21 10:40:56 +0900

    [Feat] Player Animation Apply
    플레이어 애니메이션을 만들고 오버라이드 애니메이터 컨트롤러로 캐릭터 별 애니메이터 제작

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -6,17 +6,27 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float _moveSpeed = 5f;
     private Vector2 _moveInput;
     private Rigidbody2D _playerRigid;
+    private SpriteRenderer _playerSpriteRenderer;
+    private Animator _playerAnimator;
 
 
     void Awake()
     {
         _playerRigid = GetComponent<Rigidbody2D>();
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         Vector2 dirVec = Time.deltaTime * _moveSpeed * _moveInput;
         _playerRigid.MovePosition(_playerRigid.position + dirVec);
+
+        if(dirVec.x != 0)
+        {
+            _playerSpriteRenderer.flipX = dirVec.x < 0 ? true : false; //플레이어 스프라이트 좌 우 변경
+        }
+        _playerAnimator.SetFloat("Speed", dirVec.magnitude);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
